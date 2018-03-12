870951044f7b1ef27da3e58f164e75b7b6bef36f
Dada12590 <amol12590@gmail.com>
2018-03-12 12:33:11 +0530

    Commiting all pending changes

---
+++ src/main/java/com/Dmart/App.java
@@ -0,0 +1,13 @@
+package com.Dmart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
